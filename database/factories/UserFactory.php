0355cd51d09cf09fecfbce64f9b2cd6a6d13c415
pallabcodes <kayydraws@gmail.com>
2025-04-12 17:49:22 +0530

    laravel: Resful API (basic)

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * So, it seeds `User` table/model with fake data & naming must be followed for this factory to understand: Model (User), Factory (UserFactory)
+ * If using `php artisan tinker` then it could be used like `App\Models\User::factory(10)->create();
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get("/", function () {
+    return view('welcome');
+});
+
+// used the name alias so that it could be used within a blade file using "{{  route("about") }}"
+Route::get("/about", function () {
+    return "This is about page";
+})->name("about");
+
+Route::get("/users/{id}/{slug}", function ($id, $slug) {
+    return "Hello, user " . $id . "-" . $slug;
+})->name("user.detail");
+
+
+// prefix will add i.e. "blogs" to all the routes within it and "as" will do the same for named alias 
+Route::group(["prefix" => "blogs", "as" => "blogs."], function () {
+    Route::get("/", function () {
+        return "This will shows all blogs (with pagination)";
+    })->name("show");
+
+    Route::post("/create", function () {
+        return "This is Blog create page";
+    })->name("create");
+
+    Route::put("/update", function () {
+        return "Blog update page";
+    })->name("update");
+});
+
+
+// Fallback Route (when hitting an endpoint that doesn't exist then it render/return this response from this route)
+
+Route::fallback(function () {
+    return "This is a fallback route";
+});
